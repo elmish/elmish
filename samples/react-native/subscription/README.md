7714fafabc5877721ea10947ecccff5adea2ede5
Jonathan Banashek <jbanashek@gmail.com>
2016-09-24 17:05:41 -0700

    Adding basic subscriber example

---
+++ samples/react-native/subscription/README.md
@@ -0,0 +1,12 @@
+# ReactNative subscription sample.
+This sample demonstrates how to call a command based on a subscription event.
+Each second the counter is incremented by 1.
+
+## Build
+0. Have the SDK of your target platform installed and working.
+
+1. Install [react-native](https://facebook.github.io/react-native/) and test your installation
+2. `npm install -g fable-compiler`
+3. `npm install && fable`
+4. Have your Android/iOS simulator or device started at this point 
+5. `react-native run-ios` *or* `react-native run-android` 
---
+++ samples/react-native/subscription/src/App.fs
@@ -0,0 +1,85 @@
+﻿module AwesomeApp
+
+open System
+open Fable.Core
+open Fable.Import
+open Fable.Import.ReactNative
+open Fable.Helpers.ReactNative
+open Fable.Helpers.ReactNative.Props
+
+open Fable.Elmish
+
+// Model
+
+type Msg =
+  | Increment
+  | Decrement
+  | NoOp
+
+type Model = 
+  { count:int
+    currentMsg:Msg }
+
+let init () =
+  { count = 0
+    currentMsg = NoOp }, []
+
+// Update
+
+let update (msg:Msg) (model:Model) : Model*Cmd<Msg> =
+  match msg with
+  | Increment ->
+    { model with
+        count = model.count + 1
+        currentMsg = Increment }, []
+  | Decrement ->
+    { model with
+        count = model.count - 1
+        currentMsg = Decrement }, []
+  | NoOp ->
+      model, []
+
+// Subscriptions
+let timerTick model dispatch =
+  let t = new Timers.Timer 1000.
+  t.Elapsed.Subscribe(fun _ -> dispatch Increment) |> ignore
+  t.Enabled <- true
+
+let subscription model =
+  Cmd.ofSub (timerTick model)
+
+
+// rendering views with ReactNative
+module R = Fable.Helpers.ReactNative
+
+let view {count = count} dispatch =
+  let onClick msg =
+    fun _ -> msg |> dispatch 
+
+  R.view []
+    [ Styles.button "-" (onClick Decrement)
+      R.text [] (string count)
+      Styles.button "+" (onClick Increment)
+    ]
+
+// App
+let program =
+    Program.mkProgram init update
+    |> Program.withSubscription subscription
+    |> Program.withConsoleTrace
+
+type App() as this =
+    inherit React.Component<obj, Model>()
+
+    let safeState state =
+        match unbox this.props with 
+        | false -> this.state <- state
+        | _ -> this.setState state
+
+    let dispatch = program |> Program.run safeState
+
+    member this.componentDidMount() =
+        this.props <- true
+
+    member this.render() =
+        view this.state dispatch
