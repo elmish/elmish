470297df226a6892591f4366bf932cf61f6dd446
Eugene Tolmachev <eugene.tolmachev@prolucid.ca>
2017-03-09 13:52:10 -0500

    #41 - schedule the rendering with

--- src/elmish-react/react.fs
+++ src/elmish-react/react.fs
@@ -6,14 +6,21 @@ open Fable.Core
 
 [<RequireQualifiedAccess>]
 module Program =
+    open Fable.Import.Browser
     module R = Fable.Helpers.React
 
     /// Setup rendering of root React component inside html element identified by placeholderId
     let withReact placeholderId (program:Elmish.Program<_,_,_,_>) =
+        let mutable lastRequest = None
         let setState model dispatch =
-            Fable.Import.ReactDom.render(
-                lazyView2With (fun x y -> obj.ReferenceEquals(x,y)) program.view model dispatch,
-                Fable.Import.Browser.document.getElementById(placeholderId)
-            )
+            match lastRequest with
+            | Some r -> window.cancelAnimationFrame r
+            | _ -> ()
+
+            lastRequest <- Some (window.requestAnimationFrame (fun _ -> 
+                Fable.Import.ReactDom.render(
+                    lazyView2With (fun x y -> obj.ReferenceEquals(x,y)) program.view model dispatch,
+                    document.getElementById(placeholderId)
+                )))
 
         { program with setState = setState } 
