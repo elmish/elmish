daad2136a71284b0dcec9d3f1729a7c199ef711e
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-05-16 01:09:51 +0200

    Use Paket (magic mode)

--- build.sh
+++ build.sh
@@ -2,13 +2,6 @@
 if test "$OS" = "Windows_NT"
 then
   # use .Net
-
-  .paket/paket.bootstrapper.exe
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
   .paket/paket.exe restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
@@ -18,12 +11,6 @@ then
   packages/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx
 else
   # use mono
-  mono .paket/paket.bootstrapper.exe
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
   mono .paket/paket.exe restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
