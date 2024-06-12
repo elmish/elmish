a94c8cfd6623cecc59bb645a4e264a041c29b9b1
Eugene Tolmachev <eugene.tolmachev@microsoft.com>
2024-06-12 13:07:10 -0400

    reintroduce try/catch in the processing

--- tests/ProgramTest.fs
+++ tests/ProgramTest.fs
@@ -1,5 +1,6 @@
 module Elmish.ProgramTests
 
+open NUnit.Framework
 open FsCheck.NUnit
 open Swensen.Unquote
 open Elmish
@@ -10,16 +11,14 @@ type Msg =
   | Decrement
   | Increment10Times
 
-let init msgs = 0, msgs |> List.map Cmd.ofMsg |> Cmd.batch
-
-let update msg m =
-  match msg with
-  | Increment -> m + 1, Cmd.none
-  | Decrement -> m - 1, Cmd.none
-  | Increment10Times -> m, Cmd.batch [ for _ in 1..10 -> Cmd.ofMsg Increment ] 
-
 [<Property(MaxTest = 10, EndSize = 100)>]
 let dispatchesBatch (msgs: Msg list) =  
+    let init msgs = 0, msgs |> List.map Cmd.ofMsg |> Cmd.batch
+    let update msg m =
+        match msg with
+        | Increment -> m + 1, Cmd.none
+        | Decrement -> m - 1, Cmd.none
+        | Increment10Times -> m, Cmd.batch [ for _ in 1..10 -> Cmd.ofMsg Increment ] 
     printfn "Folding..."
     let expected =
       (0, msgs)
@@ -33,3 +32,20 @@ let dispatchesBatch (msgs: Msg list) =
     } |> Async.Start
     System.Threading.Thread.Sleep (1_000)
     counted =! expected
+
+[<Test>]
+let ``update throwing exception should not crash the program`` () =
+    let o = obj()
+    let syncDispatch dispatch = lock o (fun () -> dispatch)
+    let init _ = 0, Cmd.OfAsync.perform async.Return Increment id
+    let update _ _ = failwith "Boom!"
+    let view _ = ignore
+    let mutable called = false
+    let onError _ = called <- true
+    Program.mkProgram init update view
+    |> Program.withConsoleTrace
+    |> Program.mapErrorHandler (fun _ -> onError)
+    |> Program.runWithDispatch syncDispatch ()
+    System.Threading.Thread.Sleep (1_000)
+    called =! true
+
